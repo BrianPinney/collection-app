920d63f45f216899674c7ddfddc540eef0d3b203
Brian J. Pinney <brianj.pinney@gmail.com>
2023-05-01 16:39:14 -0400

    First commit movie collection

---
+++ src/components/Gallery/Panel.jsx
@@ -0,0 +1,19 @@
+import { Card, Col, Image } from "react-bootstrap";
+
+export default function Panel({ data: { id, episode, content, image } }) {
+  
+    return (
+    <Col key={id} sm={10} md={6} lg={4}>
+      <Card className="p-3">
+        <figure>
+          <Image src={image} fluid />
+        </figure>
+        
+        <div className="episode">
+          <h3>{episode}</h3>
+          <p>{content}</p>
+        </div>
+      </Card>
+    </Col>
+  );
+}
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,19 @@
+import { HashLink } from "react-router-hash-link";
+import { Button, Col, Container, Row } from "react-bootstrap";
+
+export default function Main(){
+
+    return (
+        <main>
+            <Container>
+                <Row>
+                    <Col>
+                    <h1>Gallery</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempora recusandae ipsam laborum ea aperiam asperiores dolores accusantium animi placeat, maiores molestias. Praesentium, ad neque. Officiis velit provident illum! Ad, cupiditate.</p>
+                    <HashLink to="/#gallery"><Button>See Gallery</Button></HashLink>
+                    </Col>
+                </Row>
+            </Container>
+        </main>
+    )
+}
